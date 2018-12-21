2768c5ccb9929a32f0e1fcc3b7f6e8d1cebbdaa0
Francesco de Pertis <f.depertis@studenti.unisa.it>
2018-12-21 19:45:44 +0100

    new timetable

--- pkg_1/list_routes.py
+++ pkg_1/list_routes.py
@@ -22,10 +22,18 @@ def list_routes(t, a, b, actual_time, max_time):
         raise TypeError("actual_time must be datetime.time typed.")
     if type(max_time) is not datetime.timedelta:
         raise TypeError("max_time must be datetime.timedelta typed.")
-    discovery = {}
-    for f in t.incident_edges(a):
-        if f.l() > actual_time + a.c() and f.a() < max_time:
-            o = f.opposite(a)
+    routes = dict()
+    _list_routes_aux(t, a, b, actual_time, max_time, routes)
+    return routes
+
 
 def _list_routes_aux(t, a, b, actual_time, max_time, routes):
-    pass
+    if a == b:
+        return
+    else:
+        for f in t.incident_edges(a):
+            o = f.opposite(a)
+            routes[o] = f
+            if f.l() > actual_time + a.c() or f.a() < max_time:
+                _list_routes_aux(t, o, b, o.a(), max_time, routes)
+
