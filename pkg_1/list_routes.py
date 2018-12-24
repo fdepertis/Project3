2ff2c839642dc6f6d859ef7d0b80f0f7ef39eff5
Antonino95 <43966341+Antonino95@users.noreply.github.com>
2018-12-24 12:00:02 +0100

    Add files via upload

--- pkg_1/list_routes.py
+++ pkg_1/list_routes.py
@@ -9,30 +9,69 @@ considerare che non è possibile effettuare una coincidenza se tra l’orario di
 atterraggio di un volo ed il tempo di decollo del volo successivo intercorre un tempo
 inferiore a c(a).
 """
-
-from ..timetable import Timetable
 import datetime
 
-def list_routes(t, a, b, actual_time, max_time):
-    if type(t) is not Timetable:
-        raise TypeError("t must be Timetable typed.")
-    if type(a) is not type(b) is not Timetable.Airport:
-        raise TypeError("a and b must be Timetable.Airport typed.")
-    if type(actual_time) is not datetime.time:
-        raise TypeError("actual_time must be datetime.time typed.")
-    if type(max_time) is not datetime.timedelta:
-        raise TypeError("max_time must be datetime.timedelta typed.")
-    routes = dict()
-    _list_routes_aux(t, a, b, actual_time, max_time, routes)
-    return routes
+from Project3.airline_company import airline_company
+from TdP_collections.stack.array_stack import ArrayStack
+from TdP_collections.tree.linked_binary_tree import LinkedBinaryTree
+from Project3.timetable import Timetable
 
 
-def _list_routes_aux(t, a, b, actual_time, max_time, routes):
-    if a == b:
-        return
-    else:
-        for f in t.incident_edges(a):
-            o = f.opposite(a)
-            routes[o] = f
-            if f.l() > actual_time + a.c() or f.a() < max_time:
-                _list_routes_aux(t, o, b, o.a(), max_time, routes)
+def list_routes(timetable,a,b,t,T):
+
+    if t + T <= list(timetable.incident_flights(a))[0].l():return None
+    else: return list(dfs_paths(timetable, a, b, t, T,a))
+
+
+
+def dfs_paths(timetable, start, goal, t, T, current, path_flights = None):
+
+    if path_flights is None:
+        path_flights = []
+
+    if current == goal:
+        if path_flights[0].l() >= t and path_flights[len(path_flights)-1].l() <= t + T:
+            yield path_flights
+
+    for next in set(timetable.incident_flights(current)) - set(path_flights):
+        if next.s() != start and path_flights[len(path_flights)-1].a() + next.s().c() > next.l():
+            print("ECCO IL VOLO CON CUI SEI ARRIVATO NEL TUO ULTIMO AEREOPORTO")
+            print(path_flights[len(path_flights)-1])
+            print("ECCO IL VOLO CHE NON PUOI PRENDERE")
+            print(next)
+            print("NON LO PUOI PRENDERE PERCHE'")
+            print(str(path_flights[len(path_flights)-1].a()) + " + " + str(next.s().c()) + " <= " +  str(next.l()))
+            print("")
+        if (next.s() != start and path_flights[len(path_flights)-1].a() + next.s().c() > next.l()):
+            continue
+        if next.l() < t + T:
+            yield from dfs_paths(timetable, start, goal, t, T, next.opposite(current), path_flights + [next])
+
+
+if __name__ == '__main__':
+    timetable = Timetable()
+    a = timetable.insert_airport("A")
+    b = timetable.insert_airport("B",datetime.timedelta(hours=1))
+    c = timetable.insert_airport("C")
+    d = timetable.insert_airport("D")
+    e = timetable.insert_airport("E")
+    f = timetable.insert_airport("F")
+
+    timetable.insert_flight(a,b,datetime.datetime(2018,12,22,8,0,0),datetime.datetime(2018,12,22,8,15,0),9)
+    timetable.insert_flight(a,b,datetime.datetime(2018,12,22,8,0,0),datetime.datetime(2018,12,22,8,20,0),9)
+    timetable.insert_flight(a,c,datetime.datetime(2018,12,22,8,0,0),datetime.datetime(2018,12,22,9,0,0),9)
+    timetable.insert_flight(a,c,datetime.datetime(2018,12,22,11,0,0),datetime.datetime(2018,12,22,12,0,0),9)
+    timetable.insert_flight(b,d,datetime.datetime(2018,12,22,8,15,0),datetime.datetime(2018,12,22,9,0,0),9)
+    timetable.insert_flight(b,e,datetime.datetime(2018,12,22,8,15,0),datetime.datetime(2018,12,22,9,0,0),9)
+    timetable.insert_flight(b,e,datetime.datetime(2018,12,22,8,20,0),datetime.datetime(2018,12,22,9,0,0),9)
+    timetable.insert_flight(e,f,datetime.datetime(2018,12,22,9,0,0),datetime.datetime(2018,12,22,10,0,0),9)
+    timetable.insert_flight(c,f,datetime.datetime(2018,12,22,9,0,0),datetime.datetime(2018,12,22,10,0,0),9)
+    timetable.insert_flight(c,f,datetime.datetime(2018,12,22,14,0,0),datetime.datetime(2018,12,22,15,0,0),9)
+    
+    for i in list_routes(timetable,a,f,datetime.datetime(2018,12,22,7,0,0),datetime.timedelta(hours=3)):
+        print("[ ")
+        for k in i:
+            print(str(k) + " ,")
+        print(" ]")
+
+
